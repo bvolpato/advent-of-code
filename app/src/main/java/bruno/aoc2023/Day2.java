c7316b2372d6f55bc72977a6371baee07bb2de97
Bruno Volpato <bvolpato@google.com>
2023-12-06 20:32:44 -0500

    Day 6, easy

--- app/src/main/java/bruno/aoc2023/Day2.java
+++ app/src/main/java/bruno/aoc2023/Day2.java
@@ -4,57 +4,58 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.brunocvcunha.inutils4j.MyStringUtils;
 
 public class Day2 {
 
-  private static Map<String, Integer> available = new HashMap<>();
+    private static Map<String, Integer> available = new HashMap<>();
 
-  static {
-    available.put("red", 12);
-    available.put("green", 13);
-    available.put("blue", 14);
-  }
-
-  public static void main(String[] args) throws IOException, UnsupportedFlavorException {
-    String clipboard = MyStringUtils.getFromClipboard();
-    System.out.println("Clipboard content: " + clipboard);
-
-    int ans1 = 0;
-    int ans2 = 0;
-
-    for (String line : MyStringUtils.asListLines(clipboard)) {
-      int gameNum = Integer.valueOf(line.split(":")[0].split(" ")[1]);
-      String lineContent = line.split(":")[1].trim();
-
-      boolean good = true;
-
-      Map<String, Integer> maxPerColorTurns = new HashMap<>();
-
-      for (String turn : lineContent.split(";")) {
-        for (String count : turn.split(",")) {
-          int quantity = Integer.valueOf(count.trim().split(" ")[0]);
-          String color = count.trim().split(" ")[1];
-
-          maxPerColorTurns.put(
-              color, Integer.max(maxPerColorTurns.getOrDefault(color, 0), quantity));
-
-          if (quantity > available.get(color)) {
-            good = false;
-          }
-        }
-      }
-
-      if (good) {
-        ans1 += gameNum;
-      }
-
-      int asInt = maxPerColorTurns.values().stream().reduce(1, (a, b) -> a * b);
-      System.out.println(maxPerColorTurns + " -> " + asInt);
-      ans2 += asInt;
+    static {
+        available.put("red", 12);
+        available.put("green", 13);
+        available.put("blue", 14);
     }
 
-    System.out.println(ans1);
-    System.out.println(ans2);
-  }
+    public static void main(String[] args) throws IOException, UnsupportedFlavorException {
+        String clipboard = MyStringUtils.getFromClipboard();
+        System.out.println("Clipboard content: " + clipboard);
+
+        int ans1 = 0;
+        int ans2 = 0;
+
+        for (String line : MyStringUtils.asListLines(clipboard)) {
+            int gameNum = Integer.valueOf(line.split(":")[0].split(" ")[1]);
+            String lineContent = line.split(":")[1].trim();
+
+            boolean good = true;
+
+            Map<String, Integer> maxPerColorTurns = new HashMap<>();
+
+            for (String turn : lineContent.split(";")) {
+                for (String count : turn.split(",")) {
+                    int quantity = Integer.valueOf(count.trim().split(" ")[0]);
+                    String color = count.trim().split(" ")[1];
+
+                    maxPerColorTurns.put(
+                            color, Integer.max(maxPerColorTurns.getOrDefault(color, 0), quantity));
+
+                    if (quantity > available.get(color)) {
+                        good = false;
+                    }
+                }
+            }
+
+            if (good) {
+                ans1 += gameNum;
+            }
+
+            int asInt = maxPerColorTurns.values().stream().reduce(1, (a, b) -> a * b);
+            System.out.println(maxPerColorTurns + " -> " + asInt);
+            ans2 += asInt;
+        }
+
+        System.out.println(ans1);
+        System.out.println(ans2);
+    }
 }
---
+++ app/src/main/java/bruno/aoc2023/Day6.java
@@ -0,0 +1,55 @@
+package bruno.aoc2023;
+
+import org.brunocvcunha.inutils4j.MyStringUtils;
+
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day6 {
+
+    public static void main(String[] args) throws IOException, UnsupportedFlavorException {
+        String clipboard = MyStringUtils.getFromClipboard();
+        System.out.println("Clipboard content: " + clipboard);
+
+        List<String> listLines = MyStringUtils.asListLines(clipboard);
+
+        String num1 = listLines.get(0).split(":")[1].trim();
+        String num2 = listLines.get(1).split(":")[1].trim();
+
+        // part 2
+        num1 = num1.replace(" ", "");
+        num2 = num2.replace(" ", "");
+
+        long[] time = Arrays.stream(num1.split("\\s+")).mapToLong(Long::valueOf).toArray();
+        long[] distance = Arrays.stream(num2.split("\\s+")).mapToLong(Long::valueOf).toArray();
+
+        System.out.println(Arrays.toString(time));
+        System.out.println(Arrays.toString(distance));
+
+        int ans = 1;
+        for (int i = 0; i < time.length; i++) {
+
+            int ways = 0;
+
+            for (int speed = 0; speed < time[i]; speed++) {
+                long timeLeft = time[i] - speed;
+                long totalDist = speed * timeLeft;
+                if (totalDist > distance[i]) {
+                    ways++;
+                }
+
+                // System.out.println("Speed is " + speed + ", time left is " + timeLeft + " so total dist was " + totalDist);
+            }
+
+            System.out.println("time: " + time[i] + ", dist: " + distance[i] + " -> " + ways + " ways.");
+            ans *= ways;
+        }
+
+
+        System.out.println(ans);
+    }
+
+
+}
